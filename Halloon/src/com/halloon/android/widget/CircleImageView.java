c86c893a651759c680d69f810d0d1f260a85ffe7
xxxzhi <xxxzhi91@gmail.com>
2014-03-11 00:45:30 +0800

    repair imageview quality

--- Halloon/src/com/halloon/android/widget/CircleImageView.java
+++ Halloon/src/com/halloon/android/widget/CircleImageView.java
@@ -14,8 +14,10 @@ import android.graphics.Path;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -84,30 +86,116 @@ public class CircleImageView extends ImageView {
 	// canvas.restoreToCount(saveCount);
 	// }
 	//
+	boolean hasSetImg = true;
+	
+	@Override
+	public void setImageResource(int resId) {
+		super.setImageResource(resId);
+		hasSetImg = true;
+	}
+
+	@Override
+	public void setImageURI(Uri uri) {
+		super.setImageURI(uri);
+		hasSetImg = true;
+	}
+
+	@Override
+	public void setImageDrawable(Drawable drawable) {
+		super.setImageDrawable(drawable);
+		hasSetImg = true;
+	}
+
+	@Override
+	public void setImageBitmap(Bitmap bm) {
+		super.setImageBitmap(bm);
+		hasSetImg = true;
+	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-
-		Drawable drawable = getDrawable();
-
-		if (drawable == null) {
-			return;
+		if(hasSetImg){
+			Drawable drawable = getDrawable();
+	
+			if (drawable == null) {
+				return;
+			}
+	
+			if (getWidth() == 0 || getHeight() == 0) {
+				return;
+			}
+	
+			Bitmap b = ((BitmapDrawable) drawable).getBitmap();
+	
+	
+			int w = getMeasuredWidth(), h = getMeasuredHeight();
+//			Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
+//			Bitmap roundBitmap = getCroppedBitmap(bitmap, w > h ? h : w);
+	//		canvas.drawBitmap(roundBitmap, 0, 0, null);
+			setImageBitmap(toCircleBitmap(b));
+			hasSetImg = false;
 		}
-
-		if (getWidth() == 0 || getHeight() == 0) {
-			return;
-		}
-
-		Bitmap b = ((BitmapDrawable) drawable).getBitmap();
-		Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-
-		int w = getWidth(), h = getHeight();
-
-		Bitmap roundBitmap = getCroppedBitmap(bitmap, w);
-		canvas.drawBitmap(roundBitmap, 0, 0, null);
-
+		super.onDraw(canvas);
+		
 	}
 
+	
+	public static Bitmap toCircleBitmap(Bitmap bmp){
+		Bitmap sbmp;
+		int w = bmp.getWidth(),h = bmp.getHeight();
+		
+		//diameter
+		int dia = w > h ?h :w; 
+		
+		if(w != h){
+			sbmp = Bitmap.createBitmap(bmp, (w-dia)/2, (h-dia)/2, dia, dia);
+		}else{
+			sbmp = bmp;
+		}
+//		
+		Bitmap output = Bitmap.createBitmap(sbmp.getWidth(), sbmp.getHeight(),
+				sbmp.getConfig());
+		Canvas canvas = new Canvas(output);
+
+		final Paint paint = new Paint();
+		final Rect rect = new Rect(0, 0, sbmp.getWidth(), sbmp.getHeight());
+
+		paint.setAntiAlias(true);
+		paint.setFilterBitmap(true);
+		paint.setDither(true);
+		canvas.drawARGB(0, 0, 0, 0);
+		paint.setColor(Color.parseColor("#BAB399"));
+		canvas.drawCircle(sbmp.getWidth() / 2 + 0.7f,
+				sbmp.getHeight() / 2 + 0.7f, sbmp.getWidth() / 2 + 0.1f, paint);
+		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+		canvas.drawBitmap(sbmp, rect, rect, paint);
+
+		return output;
+	}
+	
+	public static Bitmap toRoundCorner(Bitmap bitmap, float ratio) {
+        System.out.println("图片是否变成圆形模式了+++++++++++++");
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawRoundRect(rectF, bitmap.getWidth() / ratio,
+                bitmap.getHeight() / ratio, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        System.out.println("pixels+++++++" + String.valueOf(ratio));
+
+        return output;
+
+    }
+	
 	public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
 		Bitmap sbmp;
 		if (bmp.getWidth() != radius || bmp.getHeight() != radius)
